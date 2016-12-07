9141f4d03cd5b7a5f051f2c0b6c73f2f673710a9
mvdbeek <m.vandenbeek@gmail.com>
2016-12-07 14:50:17 +0100

    Fix erroneous conversion of Dm88 to X-element

--- conversion_files/conversion.sh
+++ conversion_files/conversion.sh
@@ -1,19 +1,21 @@
  sed -i "s|\sR2$|\tR2-element|g" dm6_FBti_to_Families.tab
+ sed -i "s|\sFW$|\tDmel\\\F-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\s1360$|\tDmel\\\1360|g" dm6_FBti_to_Families.tab
  sed -i "s|\sMax$|\tDmel\\\Max-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\sDoc2$|\tDmel\\\Doc2-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\sS$|\tDmel\\\S-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\sG$|\tDmel\\\G-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\saurora$|\taurora-element|g" dm6_FBti_to_Families.tab
- sed -i "s|\sDoc4$|\tDmel\\\Doc4|g" dm6_FBti_to_Families.tab
+ sed -i "s|\sDoc4$|\tDmel\\\Doc4-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\sR2$|\tDmel\\\R2-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\s412$|\tF-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\s412$|\tDmel\\\412|g" dm6_FBti_to_Families.tab
  sed -i "s|\sH$|\thobo|g" dm6_FBti_to_Families.tab
  sed -i "s|\sY$|\tDmel\\\Y-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\sI$|\tDmel\\\I-element|g" dm6_FBti_to_Families.tab
+ sed -i "s|\sX$|\tDmel\\\X-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\s297$|\tDmel\\\297|g" dm6_FBti_to_Families.tab
  sed -i "s|\sR1$|\tDmel\\\R1A1-element|g" dm6_FBti_to_Families.tab
- sed -i "s|\sDM88$|\tX-element|g" dm6_FBti_to_Families.tab
+ sed -i "s|\sR1A1$|\tDmel\\\R1A1-element|g" dm6_FBti_to_Families.tab
  sed -i "s|\sDM88$|\tDmel\\\Dm88|g" dm6_FBti_to_Families.tab
  sed -i "s|\s1731$|\tDmel\\\1731|g" dm6_FBti_to_Families.tab
